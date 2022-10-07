220303737c8aec1aef21292cb4df46e736e74ee9
Dalyll Reguia <dalyll.reguia@ynov.com>
2022-10-07 13:28:20 +0200

    system to pick random word added

--- hangman.go
+++ hangman.go
@@ -1,1 +1,66 @@
 package Hangman
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+func Game() {
+	data, err := os.Open("words.txt")
+	if err != nil {
+		log.Panicf("failed reading data from file: %s", err)
+	}
+	var str []string
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		str = append(str, scanner.Text())
+	}
+	x1 := rand.NewSource(time.Now().UnixNano())
+	y1 := rand.New(x1)
+	random := str[y1.Intn(len(str))]
+
+	fmt.Println(random)
+
+	n := len(random)/2 - 1
+	print(n)
+	print("\n")
+	var randomSplitted []string
+	var ToShow []string
+	randomSplitted = strings.Split(random, "")
+	print(randomSplitted[0])
+	print("\n")
+	for i := 0; i < n; i++ {
+		RandomRune := []rune(random)
+		randomIndex := rand.Intn(len(RandomRune))
+		pick := RandomRune[randomIndex]
+		ToShow = append(ToShow, string(pick))
+	}
+	fmt.Println(ToShow)
+	res := make([]string, len(randomSplitted))
+	for i := 0; i < len(randomSplitted); i++ {
+		res[i] = "-"
+	}
+	fmt.Println(res)
+	count := 0
+	for _, i := range randomSplitted {
+		for _, j := range ToShow {
+			if count <= len(randomSplitted) {
+				if i == j {
+					res[count] = j
+				} else {
+					res[count] = "-"
+				}
+				count++
+			} else {
+				break
+			}
+		}
+	}
+	fmt.Println(res)
+
+}
--- test/main.go
+++ test/main.go
@@ -1,1 +1,7 @@
 package main
+
+import "Hangman"
+
+func main() {
+	Hangman.Game()
+}
